2b85e6f2addc5a0ccedcfd802f3d3990b84af2cd
Nain <nainberre@gmail.com>
2022-02-02 11:31:11 +0100

    Clase

---
+++ src/pruebapersona/ClaseAmigoNain.java
@@ -0,0 +1,17 @@
+package pruebapersona;
+
+public class ClaseAmigoNain {
+
+	public static void main(String[] args) {
+int persona;
+int num=3;
+int i=0;
+if (i>num) {
+	System.out.println("buenas");
+}else {
+	System.out.println("malas");
+}
+	}
+
+}
+
